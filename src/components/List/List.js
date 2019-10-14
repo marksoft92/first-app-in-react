2f1e4ba12f849240e7dbc7dd55b77804b1cc064f
Dawid Bienkowski <marksoft1992@gmail.com>
2019-10-14 12:38:59 +0200

    new components List & Hero

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from './List.scss'
 import Hero from '../Hero/Hero.js'
+import PropTypes from 'prop-types';
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    image: PropTypes.string,
+    children: PropTypes.node.isRequired,
+  }
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-         <Hero />
+        <Hero titleText={this.props.title} />
+        <Hero titleImage={this.props.image} />
+        <div className={styles.description}>
+  {this.props.children}
+</div>
         </section>
     )
   }
