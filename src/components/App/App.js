f353bbf88ed15666ba4c2c5cc6159a92171ca53a
Dawid Bienkowski <marksoft1992@gmail.com>
2019-10-14 14:02:13 +0200

    new components List & Hero vol.2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,17 @@ import styles from './App.scss'
 import List from '../List/List.js'
 
 
+
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
          <h1 className={styles.title}>My first React app</h1>
  
-  <List image={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"}> </List>
-  <List title={['Things to do ', <sup key='1'>soon!</sup>]}>
+  <List image={"http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"} title={['Things to do ', <sup key='1'>soon!</sup>]}> </List>
   
-</List>
+  
+
   
         </main>
     )
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './List.scss'
 import Hero from '../Hero/Hero.js'
+import Column from '../Column/Column.js'
 import PropTypes from 'prop-types';
 class List extends React.Component {
   static propTypes = {
@@ -15,10 +16,16 @@ class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} />
-        <Hero titleImage={this.props.image} />
+        <Hero titleText={this.props.title} titleImage={this.props.image}  />
+    <h3 className={styles.title}>Sekcja z Kolumnami</h3>
         <div className={styles.description}>
+          
   {this.props.children}
+</div>
+<div className={styles.columns}>
+  <Column title="Animals" />
+  <Column title="Plants" />
+  <Column title="Minerals" />
 </div>
         </section>
     )
